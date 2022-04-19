0e54bc277b07bc217cd76e2664ad7ff1eadbca23
AndromedaDreaming <c.collup3@gmail.com>
2022-04-19 14:06:04 +1200

    Add script for calc and operate functions

---
+++ script.js
@@ -0,0 +1,37 @@
+// Add function
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+//Subtract function
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+//Multiply function
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+//Divide function
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+//Operate function that takes input and calls an above function
+function operate(mum1, num2, operand) {
+    switch (operand) {
+        case '-':
+            return num1 - num2;
+            break;
+        case '*':
+            return num1 * num2;
+            break;    
+        case '/':
+            return num1 / num2;
+            break;
+        default:
+            return num1 + num2;    
+    }
+}
+console.log(operate);
